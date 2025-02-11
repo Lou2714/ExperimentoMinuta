39ae6ccaf65252aaf8858e2c0c7703c93ebf4377
Lourdes Jiménez <00090220@uca.edu.sv>
2025-02-11 11:09:19 -0600

    :bulb: Añadí comentarios

--- Experimento_1/prueba1.cpp
+++ Experimento_1/prueba1.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int imprimirAsterisco(int times){
-    for (int i = 1; i <= times; i++)
+int imprimirAsterisco(int filas){
+    //Simboliza las filas de las matriz
+    for (int i = 1; i <= filas; i++)
     {
+        //Mientras el valor de j sea menos que i, va a imprimir j-veces el *, simboliza columnas
         for (int j = 0; j < i; j++)
         {
             cout << "*";
         }
+        //Salto de linea
         cout << "\n";
     }
 }
